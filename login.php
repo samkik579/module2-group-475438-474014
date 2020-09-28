6e6267b2e7d4410dafbeb63f6f2d95c652ca5ed6
Runyugu <runyugu@wustl.edu>
2020-09-27 20:16:40 -0500

    test

---
+++ login.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+<?php
+$h = fopen("users.txt", "r");
+$user = $_POST['username'];
+while( !feof($h) ){
+	if(trim(fgets($h)) == $user){
+
+	}
+	else{
+		$linenum++;
+	}
+}
+echo "ERROR";
+fclose($h);
+?>
+
+</body>
+</html>

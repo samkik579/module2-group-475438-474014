3ffd4aedcf26f312d3ce605b352b6ffedf4dbb1f
Xuanxuan65 <54519188+Xuanxuan65@users.noreply.github.com>
2020-09-28 23:06:59 -0500

    display

--- display.php
+++ display.php
@@ -1,23 +1,30 @@
-<?php
-session_start();
 
-$filename = $_SESSION['f'];
-echo $filename;
-$username = $_SESSION['username'];
+<!DOCTYPE html>
+<html>
+    <head>
+	    <title>ReadPage</title>
+    </head>
+    <?php
+    session_start();
 
-$full_path = sprintf("/srv/uploads/%s/%s", $username, $filename);
+    $filename = $_SESSION['f'];
 
-// Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
-$finfo = new finfo(FILEINFO_MIME_TYPE);
-$mime = $finfo->file($full_path);
+    $username = $_SESSION['uname'];
 
-echo $finfo;
-echo "ggg";
-echo $mime;
+    $full_path = sprintf("/srv/uploads/%s/%s", $username, $filename);
 
-// Finally, set the Content-Type header to the MIME type of the file, and display the file.
-header("Content-Type: ".$mime);
-header('content-disposition: inline; filename="'.$filename.'";');
-readfile($full_path);
+    // Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    $mime = mime_content_type ($filename);
 
-?>
+    header("Content-Type: ".$mime);
+    header('content-disposition: inline; filename="'.$filename.'";');
+    readfile($full_path);
+
+    ?>
+
+
+    <form action = "list.php" method = "post">
+        <input type = "submit" value = "Go Back">
+    </form>
+</html>
